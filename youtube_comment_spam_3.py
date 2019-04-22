0dc59185eedea92012af74704563780d8956c69c
oshinanika <oshin33anika@gmail.com>
2019-04-23 04:05:07 +0600

    Readme completed and added comments to 3 and 4

--- youtube_comment_spam_3.py
+++ youtube_comment_spam_3.py
@@ -1,12 +1,20 @@
 from youtube_comment_spam_2 import *
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-count_vect= CountVectorizer(decode_error='ignore')
+count_vect= CountVectorizer(decode_error='ignore') #if there is a characted outside of unicoding.. the decode error will be ignored
+
+#train(fit) and transform the training set
+#count the terms
 x_train_count=count_vect.fit_transform(x_train)
 
+#frequency of each term in each document
 tfidf_trans=TfidfTransformer()
 x_train_trans=tfidf_trans.fit_transform(x_train_count)
 
+#learns from the training set to be applied to the test set.. so no need for fit
+#count_vect is trained now so use it on test set
 x_test_count=count_vect.transform(x_test)
+
+#tfidf_trans is trained now
 x_test_trans=tfidf_trans.transform(x_test_count)
-print(x_test_trans)
+ #print(x_test_trans)
--- youtube_comment_spam_4.py
+++ youtube_comment_spam_4.py
@@ -3,8 +3,10 @@ from youtube_comment_spam_3 import *
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
 
+#n_estimators are level of decision tree
 clf=XGBClassifier(n_estimators=500)
 
+#apply the labels based on the tfidftransformer on testing set
 clf.fit(x_train_trans, y_train)
 y_pred=clf.predict(x_test_trans)
 
